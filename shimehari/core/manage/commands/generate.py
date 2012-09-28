647fca552534d155225d6ce11765ecafa2fb6745
Yamaguchi Eikichi <megane@glasses-factory.net>
2012-09-28 23:24:27 +0900

    〆張鶴gkgk
    peppep

--- shimehari/core/manage/commands/generate.py
+++ shimehari/core/manage/commands/generate.py
@@ -129,12 +129,11 @@ class Command(CreatableCommand):
 
         with open(old, 'r') as template:
             content = template.read()
-            
+
             if '%s' in content:
                 content = content % name
                 content = content[3:]
-                content = content[:len(content) - 4]
-                
+                content = content[:len(content) - 4]                
         with open(new, 'w') as newFile:
             newFile.write(content)
         sys.stdout.write("Genarating New Controller: %s\n" % new)
