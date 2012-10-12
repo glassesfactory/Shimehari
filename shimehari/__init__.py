b176484586d4e7c730e9bd684a86db311ef26998
Yamaguchi Eikichi <megane@glasses-factory.net>
2012-10-12 20:04:29 +0900

    〆張鶴gkgk
    0.1.8

--- shimehari/__init__.py
+++ shimehari/__init__.py
@@ -7,6 +7,8 @@ from werkzeug.utils import redirect
 
 from .app import Shimehari
 from .controllers import ApplicationController
+from .configuration import ConfigManager
+from .logging import createLogger
 from .helpers import jsonAvailable, json, urlFor, jsonify, sendFile, flash, getFlashedMessage
 from .routing import Router, Resource, RESTfulRule, Root, Group
 from .shared import request, session, shared, _requestContextStack, _appContextStack, currentApp
@@ -14,7 +16,7 @@ from .template import renderTemplate, renderTempalteString
 from .wrappers import Request, Response
 from .cache import Cache
 
-VERSION = '0.1.7b'
+VERSION = '0.1.8'
 AUTHOR = 'Yamaguchi Eikichi, Keiichiro Matsumoto'
 
 
