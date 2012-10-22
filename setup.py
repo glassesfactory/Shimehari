f8feb36da5db911ac4028cb6f507031a55aac7af
Yamaguchi Eikichi <megane@glasses-factory.net>
2012-10-22 14:11:26 +0900

    〆張鶴gkgk
    0.1.10

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ class runAudit(Command):
 
 setup(
     name='Shimehari',
-    version='0.1.9',
+    version='0.1.10',
     url='http://shimehari.hageee.net',
     license='BSD',
     author='Yamaguchi Eikichi, Keiichiro Matsumoto',
